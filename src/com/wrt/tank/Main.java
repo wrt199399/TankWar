38a7469b6ce156d77b5911574588518ef07ac526
wrt <nmwhwrt@126.com>
2020-05-12 20:02:09 +0800

    day11

--- src/com/wrt/tank/Main.java
+++ src/com/wrt/tank/Main.java
@@ -7,9 +7,9 @@ import java.awt.event.WindowEvent;
 public class Main {
     public static void main(String[] args) {
         TankFrame tf = new TankFrame();
-
+        int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankNum")) ;
         //初始化敌方坦克
-        for(int i=0;i<5;i++){
+        for(int i=0;i<initTankCount;i++){
             tf.tanks.add(new Tank(50+i*50,100,Dir.DOWN,Group.BAD,tf));
         }
 
---
+++ src/com/wrt/tank/PropertyMgr.java
@@ -0,0 +1,24 @@
+package com.wrt.tank;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyMgr {
+    static Properties props = new Properties();
+    static {
+
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object get(String key){
+        if(props == null){return null;}
+        return props.get(key);
+    }
+
+
+}
